a8550f86cc5d60dea8e14916d483cb804e433d06
Aaron Nartey <aaronnartey110@gmail.com>
2023-12-07 11:05:35 +0000

    I have added the various react files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import ProjectsPage from './projects/ProjectsPage'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className='container'>
+      <ProjectsPage />
+    </div>
+    
+  )
+}
+
+export default App
